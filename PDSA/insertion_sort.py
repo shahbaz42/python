b0630573b4949872b93cd3812c59038d3e648971
Shahbaz <shahbaz_ali@outlook.in>
2022-09-21 02:52:01 +0530

    add condition for returning if size of list is < 1

--- PDSA/insertion_sort.py
+++ PDSA/insertion_sort.py
@@ -3,13 +3,17 @@ def insertion_sort(L):
         Iterative Insertion sort
     """
     n = len(L)
+    if n<1:
+        return(L)
 
     for i in range(n):
+        # Assume L[:i] is sorted
+        # Move L[i] to correct position in L[:i]
         j = i
         while (j>0 and L[j]<L[j-1]):
             L[j-1], L[j] = L[j], L[j-1]
             j = j-1
-            
+        # Now L[:i+1] is sorted
     return(L)
 
 L = [int(x) for x in input().split()]
