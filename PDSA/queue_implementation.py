69ea0fad9b473fc6ddab65ef11a4e82c86f32b1b
Shahbaz <shahbaz_ali@outlook.in>
2022-10-08 08:02:23 +0530

    queue implementation in python

---
+++ PDSA/queue_implementation.py
@@ -0,0 +1,42 @@
+class Queue:
+    def __init__(self):
+        self.Q = []
+    
+    def __str__(self):
+        return(str(self.Q))
+
+    def isEmpty(self):
+        if len(self.Q)==0:
+            return True
+        return False
+    
+    def size(self):
+        return len(self.Q)
+
+    def addQ(self, x):
+        self.Q.append(x)
+    
+    def delQ(self):
+        if not self.isEmpty():
+            v = self.Q[0]
+            self.Q = self.Q[1:]
+            return v
+
+        raise Exception("Can't remove Queue is empty.")
+
+q1 = Queue()
+print(q1)
+
+for i in range(4):
+    q1.addQ(i)
+    print(q1)
+
+print(q1.isEmpty())
+print(q1.size())
+
+for i in range(4):
+    q1.delQ()
+    print(q1)
+
+print(q1)
+print(q1.isEmpty())
