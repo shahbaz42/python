241b9d688ce7d4763eafa5d6010b34de26dbe508
Shahbaz <shahbaz_ali@outlook.in>
2022-10-10 00:11:19 +0530

    DFS using adj list and global variables

---
+++ PDSA/graphs/DFS_adj_list_global.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+(visited, parent) =  ({}, {})
+
+def DFS_init(aList):
+    """
+    This function initialises visited and parent dictionaries
+    """
+    for i in aList.keys():
+        visited[i] = False
+        parent[i] = -1
+    return
+
+def DFS(aList, v):
+    """
+    Recursive function for Depth first search
+    returns visited and parent dictionaries
+    """
+    visited[v] = True
+
+    for k in aList[v]:
+        if not visited[k] :
+            parent[k] = v
+            DFS(aList, k)    
+    return
+
+def find_path(aList, u, v):
+    """
+    Given an adjaceny matrix aMat and starting and ending vertices u and v resp
+    prints path to reach v from u
+    """
+    DFS_init(A)
+    DFS(aList, u)
+
+    if not visited[v] :
+        return "No path Exists"
+    
+    i = v
+    path = []
+
+    while i != -1 :
+        path.append(i)
+        i = parent[i]
+        
+    path.reverse()
+
+    return " > ".join([str(x) for x in path])
+
+edges = [(0,1), (0,2), (0,4), (1,2), (3,4), (3,5), (3,6), (4,7), (5,6), (5,7), (5,8), (7,8), (8,9),
+         (1,0), (2,0), (4,0), (2,1), (4,3), (5,3), (6,3), (7,4), (6,5), (7,5), (8,5), (8,7), (9,8)]
+
+A = {}
+
+max_vertex = max([max(i) for i in edges])
+min_vertex = min([min(i) for i in edges])
+
+for i in range(min_vertex, max_vertex+1):
+    A[i] = []
+
+for (i,j) in edges :
+    A[i].append(j)
+
+print(A)
+print("Path from 0 to 9 : ", find_path(A, 0, 9))
+print("Path from 5 to 7 : ", find_path(A, 5, 7))
+print("Path from 1 to 6 : ", find_path(A, 1, 6))
